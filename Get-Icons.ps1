9db37e57eb548a7a3b3abc23e54956a4d81b3cc1
Nerixyz <nero.9@hotmail.de>
2022-02-24 16:39:49 +0100

    fix: dl script

--- Get-Icons.ps1
+++ Get-Icons.ps1
@@ -1,7 +1,4 @@
 Write-Output "Downloading night icons";
-1..27 | % -Parallel { iwr "https://www.windy.com/img/icons6/svg/$($_)_night_2.svg" -OutFile (New-Item -Path "packages/main/src/assets/weather/icons/night/$($_).svg" -Force) };
+1..27 | % -Parallel { iwr "https://www.windy.com/img/icons6/svg/$($_)_night_2.svg" -OutFile (New-Item -Path "packages/shared/assets/weather/icons/night/$($_).svg" -Force) };
 Write-Output "Downloading day icons";
-1..27 | % -Parallel { iwr "https://www.windy.com/img/icons6/svg/$($_).svg" -OutFile (New-Item -Path "packages/main/src/assets/weather/icons/day/$($_).svg" -Force) };
-
-Write-Output "Copying icons";
-Copy-Item -Path "packages/main/src/assets/weather/icons" -Recurse -Destination "packages/second/src/assets/weather";
+1..27 | % -Parallel { iwr "https://www.windy.com/img/icons6/svg/$($_).svg" -OutFile (New-Item -Path "packages/shared/assets/weather/icons/day/$($_).svg" -Force) };
